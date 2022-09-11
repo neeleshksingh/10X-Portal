4d5befc80711c48af5cac110ef33f5f0a0b0dfb6
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-09-11 13:16:26 +0530

    code

---
+++ FC module/largest_function.js
@@ -0,0 +1,31 @@
+let fs = require("fs");
+let data = fs.readFileSync('./input.txt', 'utf-8');
+let idx = 0;
+data = data.split('\n');
+
+function readLine() {
+    idx++;
+    return data[idx - 1].trim();
+}
+//-----------------------------------
+
+function largestElement(A){
+    // Code here
+	  let max = A[0];
+  for(let i =0;i<n;i++){
+    if(max < A[i]){
+      max = A[i];
+    }
+  }return max;
+}
+// --------- Do NOT edit Below this line -----------
+function ConvertToNumber(list){
+  for(let each in list){
+      list[each]=Number(list[each]);
+  } 
+}
+
+let n = parseInt(readLine());
+let A=readLine().split(" ");
+ConvertToNumber(A);
+console.log(largestElement(A));
