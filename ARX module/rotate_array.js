7d41f86525bbeef52749321ef7c44d58e6f34e06
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-09-12 14:36:51 +0530

    code

--- ARX module/rotate_array.js
+++ ARX module/rotate_array.js
@@ -10,27 +10,31 @@ function readLine() {
 
 //-----------------------------------
 
-let n = parseInt(readLine());
-let arr = [];
-let rows;
-for (let i = 0; i < n; i++) 
-{
-  rows = readLine().split(" ");
-  arr[i] = [];
-  for (let j = 0; j < rows.length; j++) 
-  {
-    arr[i][j] = parseInt(rows[j]);
-  }
+let n = parseInt(readLine());//4
+let matrix = [];
+let rotate = [];
+let trans = [];
+let row;
+for(let i=0 ; i<n ; i++){//0,1
+    matrix[i] = [];//[[][]]
+    row = readLine().split(" ");//['1','2','3'],['4','5','6']
+    for(let j=0 ; j<row.length ; j++){//0,
+        matrix[i][j] = parseInt(row[j]);//[[1,2,3],[4,5,6],[7,8,9],[10,11,12]]
+    }
 }
-for(let i=0;i<rows;i++)
-{
-    for(let j=i;j<4/2;j++)
-    {
-        if(i!=j)
-        {
-            let temp = arr[i][j];
-            arr[i][j]=arr[i][2-j-1];
-            arr[i][2-j-1]=temp;
-        }
-    }console.log(...arr[i]);
+for(let i=0; i<n; i++){//0
+    rotate[i] = [];//[[]]
+    for(let j=0 ; j<row.length ; j++){//0
+        rotate[i][j] = matrix[n-1-i][n-n+j];//[[10,11,12],[7,8,9],[4,5,6],[1,2,3]]
+    }
+}
+for(let i=0 ; i<row.length ; i++){
+    trans[i] = [];
+    for(let j=0 ; j<n ; j++){
+        trans[i][j] = rotate[j][i];//[[10,7,4,1],[11,8,5,2],[12,9,6,3]]
+    }
+}
+console.log(trans.length);
+for(let i=0 ; i<trans.length ; i++){
+    console.log(...trans[i]);
 }
