ea73afd865603f488131fd9d3823e7939047a270
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-09-13 13:30:58 +0530

    code

--- ARX module/max_product.js
+++ ARX module/max_product.js
@@ -25,7 +25,8 @@ for(let i=0;i<b;i++)
 let res = c[0];
 for(let j=0;j<a;j++)
 {
-   res = Math.max(res, c[j]);
+   c[j]=Math.abs(c[j])
+    res = Math.max(res, c[j]);
 }
 let res2 = d[0];
 for(let i=0;i<b;i++)
@@ -40,3 +41,30 @@ let a1 = res;
 let b1= res2;
 let ans = (a1*b1);
 console.log(Math.abs(ans));
+
+
+// Alternate Method
+
+/*let m = parseInt(readLine());
+let n = parseInt(readLine());
+let arr1 = readLine().split(" ").map(Number);
+let arr2 = readLine().split(" ").map(Number);
+for (let i = 0; i < arr1.length; i++) {
+  arr1[i] = Math.abs(arr1[i]);
+}
+for (let i = 0; i < arr2.length; i++) {
+  arr2[i] = Math.abs(arr2[i]);
+}
+let max = arr1[0];
+for (let i = 1; i < arr1.length; i++) {
+  if (arr1[i] > max) {
+    max = arr1[i];
+  }
+}
+let maxx = arr2[0];
+for (let i = 1; i < arr2.length; i++) {
+  if (arr2[i] > maxx) {
+    maxx = arr2[i];
+  }
+}
+console.log(max * maxx);*/
