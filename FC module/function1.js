560195d438582bd398444741959e157d76b14621
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-09-13 18:55:03 +0530

    code

--- FC module/function1.js
+++ FC module/function1.js
@@ -15,8 +15,8 @@ function add(var1, var2)
 {
     var1 = var1 + 2;           //7
     var2 = var2 + 3;           //9
-    console.log(var1);  //------------------>>7
-    console.log(var2);  //------------------>>9                
+    console.log(var1);  //-----PRINT------>>7
+    console.log(var2);  //-----PRINT-------->>9                
     return var1+var2;          //16
 }
 
@@ -24,5 +24,5 @@ let var1 = 5;
 let var2 = 6;
 
 let answer = add(var1, var2);
-console.log(var1);      //------------------>>5
-console.log(answer);   //------------------->>7+9 = 16
+console.log(var1);      //-------PRINT------->>5
+console.log(answer);   //--------PRINT------>>7+9 = 16
