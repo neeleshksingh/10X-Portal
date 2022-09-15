f3ada417b889e3659d4d7a6524250e90a6e4c575
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-09-15 23:06:29 +0530

    code

--- ARX module/sum_querry.js
+++ ARX module/sum_querry.js
@@ -11,26 +11,26 @@ function readLine() {
 //-----------------------------------
 
 let n = readLine().split(' ')
-let x = n[0]          //array size
-let y = n[1]          //query size
-let arr =[]
-for(let i = 0;i<x.length;i++)
-{
-    arr = readLine().split(' ')
-    console.log(arr)
-}
-
-//console.log(x)
-//console.log(y)
-for(let i=0;i<y.length;i++)
-{
-    let q1 = readLine().split(' ')
-    //console.log(q1)
-    let a = parseInt(q1[0])    // initial range of query
-    let b = parseInt(q1[1])    // last range of query
-    console.log(a, b)
-    let temp =arr.indexOf(a, b)
-    console.log(temp)
+let x = parseInt(n[0]) //array size
+let y = parseInt(n[1])   //query size
+let arr = readLine().split(" ")
+for(let i = 0;i<x;i++)
+{                      
+    arr[i] = parseInt(arr[i])
     
-}
-    
+}//console.log(arr)  
+for(let j=0;j<y;j++)
+    {
+        
+        let q1 = readLine().split(' ')
+       
+        let a = parseInt(q1[0])    // initial range of query
+        let b = parseInt(q1[1])    // last range of query
+        
+        //console.log(a,b)  
+        let sum=0;
+        for(let i=a;i<=b;i++)
+        {
+            sum += arr[i]
+        } console.log(sum)
+    }
