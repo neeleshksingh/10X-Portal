fcdd17e013ea44e7f3d1e2b9de5eeb5cd4a4ea34
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-09-15 14:05:27 +0530

    code

--- ARX module/Array_Leader.js
+++ ARX module/Array_Leader.js
@@ -9,20 +9,22 @@ function readLine() {
 }
 
 //-----------------------------------
-function replaceElements(arr){
-    // Implement this function   
-    let newArr = [];
-    let last = -1;
-    newArr.push(last);
-    while(arr.length>1) {
-        let temp = arr.pop();
-        last = Math.max(last, temp);
-        newArr.unshift(Math.max(last, temp))
-    }
-    return newArr;
-}
 
-// Do not edit anything below
+function replaceElements(arr, numElems)
+{
+    let array=[]
+    for (let i = 0; i < numElems; i++)
+    {
+        let j;
+        for (j = i+1; j < numElems; j++)
+        {
+            if (array[i] <=array[j])
+                break;
+        }    
+        if (j == numElems) // the loop didn't break
+            return(array[i]);
+  }
+}
 let numElems = parseInt(readLine());
 let arr = [];
 for (let index=0;index<numElems;index++){
