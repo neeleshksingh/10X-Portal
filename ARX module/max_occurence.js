cef7407dd666a105f78e16946165a5df7618529d
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-09-16 20:53:07 +0530

    code

--- ARX module/max_occurence.js
+++ ARX module/max_occurence.js
@@ -9,45 +9,48 @@ function readLine() {
 }
 
 //-----------------------------------
-let sz = parseInt(readLine()); // 5
-let arr = []; // [1,2,2,3,3]
-for(let i=0; i<sz; i++){ // i=0, 1 2 3
-   arr.push(parseInt(readLine()))
+ 
+let n = parseInt(readLine())
+let arr = []
+for(let i =0;i<n;i++)
+{
+    arr.push(parseInt(readLine()))
 }
-if(sz == 0) {
-   console.log(-1)
-} else {
-   let maxCount = 0; // 2
-let currCount = 1; // 1
-for(let i=1; i<sz; i++) {  // i=1 2 3 4
-   if(arr[i] == arr[i-1]) { //true
-       currCount ++; //
-   } else {
-       if(maxCount < currCount) { // 1 < 2
-           maxCount = currCount; // 2
-       }
-       currCount = 1;
-   }
+let max_count=0
+let ans=[]
+for(let i=0;i<n;i++)
+{
+    let count =1
+    for(let j=i+1;j<n;j++)
+    {
+        if(arr[i]==arr[j])
+        {
+            count++
+        }
+        else
+        {
+            break
+        }
+    }
+    if(count>max_count)
+    {
+        max_count = count
+        ans = []
+        ans.push(arr[i])
+    }
+    else if(count == max_count)
+    {
+        ans.push(arr[i])
+    }
 }
-if(currCount > maxCount) {
-   maxCount = currCount;
+if(ans.length == 0)
+{
+    console.log(-1)
 }
-let res = [];
-currCount = 1;
-for(let i=1; i<sz;i ++){
-       if(arr[i] == arr[i-1]) { //true //false
-           currCount ++; //3
-       } else {
-           if(maxCount == currCount) { // 3 < 2 //false
-               res.push(arr[i-1]);
-           }
-           currCount = 1;
-       }
-   }
-   if(currCount == 4) {
-       res.push(arr[sz-1]);
-   }
-   for(let i=0; i<res.length; i++) {
-       console.log(res[i])
-   }
+else 
+{
+    for(let i=0; i<ans.length;i++)
+    {
+        console.log(ans[i])
+    }
 }
