cde639016f35e64d1934be0b9fb80617e272523c
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-09-16 21:23:30 +0530

    code

--- ARX module/sum_querry.js
+++ ARX module/sum_querry.js
@@ -10,6 +10,40 @@ function readLine() {
 
 //-----------------------------------
 
+let input = readLine().split(" ")
+let n = parseInt (input [0])
+let q = parseInt (input [1])
+let arr = readLine().split (" ")
+for(let i=0;i<n; i++)
+{
+    arr[i]=parseInt (arr [i])
+}
+    let sum_array = []
+    let sum = arr [0]
+    sum_array.push (sum)
+    for(let i=1;i<n; i++)
+    {
+        sum = sum + arr[i]
+        sum_array.push (sum)
+    }
+        for(let i=0;i<q;i++)
+        {
+            let queries = readLine().split(" ")
+            let start = parseInt (queries [0])
+            let end = parseInt (queries [1])
+            if(start ==0 )
+            {
+            console. log (sum_array [end])
+            }
+            else
+            {
+                console. log (sum_array [end]-sum_array [start-1])
+            }
+        }
+
+//Alternative Method
+
+/*
 let n = readLine().split(' ')
 let x = parseInt(n[0]) //array size
 let y = parseInt(n[1])   //query size
@@ -35,3 +69,4 @@ for(let j=0;j<y;j++)
         } 
         console.log(sum)
     }
+*/
