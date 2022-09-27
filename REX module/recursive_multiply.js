bae4267ce7964a5dbacd2942bfa4953485759697
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-09-27 18:22:00 +0530

    code

--- REX module/recursive_multiply.js
+++ REX module/recursive_multiply.js
@@ -9,14 +9,16 @@ function readLine() {
 }
 // -------- Do NOT edit anything above this line ----------
 
-function multiply(n)
-{
-        if (n != 0)
-          return n + multiply(n(n - 1));
-        else
-          return n;
-      
+function multiplyRecursive(num) {
+  if (num < 10) {
+    return num;
+  }
+  let rem = num % 10;
+  return rem * multiplyRecursive(parseInt(num / 10));
 }
-
-let n = readLine().split(' ')
-console.log(multiply(n))
+let t = parseInt(readLine());
+let num = 0;
+for (let i = 0; i < t; i++) {
+  num = parseInt(readLine());
+  console.log(multiplyRecursive(Math.abs(num)));
+}
