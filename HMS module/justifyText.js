d6f36129133234c01614d36375f6cd633164b7dd
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-10-25 18:42:45 +0530

    code

---
+++ HMS module/justifyText.js
@@ -0,0 +1,11 @@
+const { captureRejectionSymbol } = require("events");
+let fs = require("fs");
+let data = fs.readFileSync('./in.txt', 'utf-8');
+let idx = 0;
+data = data.split('\n');
+
+function readLine() {
+    idx++;
+    return data[idx - 1].trim();
+}
+//------------------------------------------------
