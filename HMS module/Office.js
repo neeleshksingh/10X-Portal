797af1ab3752429c0e35bab95373b1de4cd62732
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-10-25 20:53:46 +0530

    code

---
+++ HMS module/Office.js
@@ -0,0 +1,13 @@
+let fs = require("fs");
+let data = fs.readFileSync('./in.txt', 'utf-8');
+let idx = 0;
+data = data.split('\n');
+
+function readLine() {
+    idx++;
+    return data[idx - 1].trim();
+}
+
+// -------- Do NOT edit anything above this line ----------
+let s = readLine().split(' ')
+let n = readLine().split(' ')
