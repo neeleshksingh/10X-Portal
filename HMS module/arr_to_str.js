dd7f90f0c55e162d98ede0ad9971014562923298
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-10-27 19:42:42 +0530

    code

--- HMS module/arr_to_str.js
+++ HMS module/arr_to_str.js
@@ -1,6 +1,7 @@
 let arr = [1,2,3,4]
 let str = arr.toString()
-let rev = str.split(',')
+let rev = str.split(",")
 console.log(str)
 console.log(rev)
 console.log(typeof(str))
+console.log(typeof(rev))
