475384628a4324755fb06096c568f5b87a30a624
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-10-31 20:49:34 +0530

    code

--- HMS module/Code_Words.js
+++ HMS module/Code_Words.js
@@ -1,43 +1,44 @@
-var uniqueMorseRepresentations = function (words) {
-    let morse = [
-      ".-",
-      "-...",
-      "-.-.",
-      "-..",
-      ".",
-      "..-.",
-      "--.",
-      "....",
-      "..",
-      ".---",
-      "-.-",
-      ".-..",
-      "--",
-      "-.",
-      "---",
-      ".--.",
-      "--.-",
-      ".-.",
-      "...",
-      "-",
-      "..-",
-      "...-",
-      ".--",
-      "-..-",
-      "-.--",
-      "--..",
-    ];
-    let alpha = "abcdefghijklmnopqrstuvwxyz";
-    let aTom = [];
-    words.forEach((word) => {
-      let temp = "";
-      for (let ch of [...word]) {
-        temp += morse[alpha.indexOf(ch)];
-      }
-      if (!aTom.includes(temp)) {
-        aTom.push(temp);
-      }
-    });
-    console.log(aTom);
-    return aTom.length;
-  };
+let fs = require("fs");
+let data = fs.readFileSync('./in.txt', 'utf-8');
+let idx = 0;
+data = data.split('\n');
+
+function readLine() {
+    idx++;
+    return data[idx - 1].trim();
+}
+
+// -------- Do NOT edit anything above this line ----------
+let arr=[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+let map=new Map()
+for(let i=0;i<arr.length;i++)
+{
+    map.set(String.fromCharCode(97+i),arr[i])
+}
+function MorsalCode(str){
+let c=0
+
+for(let i=0;i<str.length;i++)
+{
+ let empty=' '
+    for(let j=0;j<str[i].length;j++)
+    {
+       
+         empty+= map.get(str[i][j])
+    }
+    str[c]=empty
+    c++
+}
+let count=new Set()
+for(let i=0;i<str.length;i++)
+{
+    count.add(str[i])
+}
+return count.size
+}
+let testCases=parseInt(readLine())
+while(testCases--)
+{
+    let str=readLine().split(' ')
+    console.log(MorsalCode(str))
+}
