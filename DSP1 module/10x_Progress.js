3d7263e7b27eb881aa38957a94ff6e664850ae3b
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-10-31 11:15:18 +0530

    code

---
+++ DSP1 module/10x_Progress.js
@@ -0,0 +1,20 @@
+let fs = require("fs");
+let data = fs.readFileSync('./in.txt', 'utf-8');
+let idx = 0;
+data = data.split('\n');
+
+function readLine() {
+    idx++;
+    return data[idx - 1].trim();
+}
+// -------- Do NOT edit anything above this line ----------
+
+let name = readLine()
+let test = parseInt(readLine())
+for(let i=0;i<test;i++){
+    let marks = parseInt(readLine())
+    console.log(marks)
+}
+let querry = parseInt(readLine())
+
+
