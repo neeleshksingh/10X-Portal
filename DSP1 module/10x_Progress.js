b4b96a92ba76862c2729f71e4b474d68b9469fce
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-10-31 13:48:37 +0530

    code

--- DSP1 module/10x_Progress.js
+++ DSP1 module/10x_Progress.js
@@ -9,14 +9,51 @@ function readLine() {
 }
 // -------- Do NOT edit anything above this line ----------
 
-let name = readLine()
-let test = parseInt(readLine())
-for(let i=0;i<test;i++){
-    let marks = parseInt(readLine())
-    console.log(marks)
+class Student{
+    constructor(name)
+    {
+        this.name=name
+    }
+  get_avrg_score(arr)
+  {
+     let avrg=0,sum=0
+     for(let i=0;i<arr.length;i++)
+     {
+         sum+=arr[i]
+     }
+     avrg=Math.round(sum/size)
+     return `${this.name} average score: ${avrg}`
+  }
+  get_test_score(no,arr){
+     if(no>arr.length)
+     {
+        return `${this.name} test ${no} marks: NA`
+     }
+     return `${this.name} test ${no} marks: ${arr[no-1]}`
+  }
 }
-let querry = parseInt(readLine())
-for(let i=0;i<querry;i++){
-    let 
+
+let name=readLine()
+let obj=new Student(name)
+let size=parseInt(readLine())
+let arr=[]
+for(let i=0;i<size;i++)
+{
+    arr.push(parseInt(readLine()))
 }
+let no_of_query = parseInt(readLine())
+// console.log(no_of_query)
+while(no_of_query--)
+{
+    let q=readLine()
+    if(q=='get_marks')
+    {
+        var score_no=parseInt(readLine())
+        console.log(obj.get_test_score(score_no,arr))
+    }
+    else{
+        console.log(obj.get_avrg_score(arr))
+    }
+}
+
 
