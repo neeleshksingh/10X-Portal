e7c4b74fecab0fd492adf07337bdba62544e534c
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-10-31 20:40:52 +0530

    code

---
+++ HMS module/Code_Words.js
@@ -0,0 +1,43 @@
+var uniqueMorseRepresentations = function (words) {
+    let morse = [
+      ".-",
+      "-...",
+      "-.-.",
+      "-..",
+      ".",
+      "..-.",
+      "--.",
+      "....",
+      "..",
+      ".---",
+      "-.-",
+      ".-..",
+      "--",
+      "-.",
+      "---",
+      ".--.",
+      "--.-",
+      ".-.",
+      "...",
+      "-",
+      "..-",
+      "...-",
+      ".--",
+      "-..-",
+      "-.--",
+      "--..",
+    ];
+    let alpha = "abcdefghijklmnopqrstuvwxyz";
+    let aTom = [];
+    words.forEach((word) => {
+      let temp = "";
+      for (let ch of [...word]) {
+        temp += morse[alpha.indexOf(ch)];
+      }
+      if (!aTom.includes(temp)) {
+        aTom.push(temp);
+      }
+    });
+    console.log(aTom);
+    return aTom.length;
+  };
