cd2fa6942172de59d85ea6f7d2e722deac5d248a
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-11-02 11:14:44 +0530

    code

--- DSP1 module/second_largest_number.js
+++ DSP1 module/second_largest_number.js
@@ -8,10 +8,26 @@ function readLine() {
     return data[idx - 1].trim();
 }
 // -------- Do NOT edit anything above this line ----------
+/*
+let n = parseInt(readLine())
+let arr = []
+let max = 0
+for(let i=0;i<n;i++){
+    arr[i] = parseInt(readLine())
+    max = arr[i]
+}
+console.log(arr)
+console.log(Math.max(max))
+*/
 
+function second_highest(arr)
+{
+  var second_highest = arr.sort(function(a, b) { return b - a; })[1];
+  return second_highest;
+}
 let n = parseInt(readLine())
 let arr = []
 for(let i=0;i<n;i++){
     arr[i] = parseInt(readLine())
 }
-console.log(arr)
+console.log(second_highest(arr))
