39b53b4b1c91b3e9dd9855edfad31a653c025c0a
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-11-07 15:26:29 +0530

    code

--- SORTX module/insertion_sort.js
+++ SORTX module/insertion_sort.js
@@ -2,24 +2,20 @@ let fs = require("fs");
 let data = fs.readFileSync('./in.txt', 'utf-8');
 let idx = 0;
 data = data.split('\n');
-function readLine() 
-{
+function readLine() {
     idx++;
     return data[idx - 1].trim();
 }
 
 // -------- Do NOT edit anything above this line ----------
+
 let n = parseInt(readLine())
-let ar1 = parseInt(readLine())
-let arr1 = readLine().split(' ')
-let ar2 = parseInt(readLine())
-let arr2 = readLine().split(' ')
-for(let i=0;i<ar1;i++){
-    arr1[i] = parseInt(arr1[i])
+let arr = []
+for (let i = 0; i < n; i++) {
+    arr.push(parseInt(readLine()))
+    let arr1 = readLine().split(' ')
+    for (let j = 0; j < arr[i]; j++) {
+        arr1[j] = parseInt(arr1[j])
+    }
+    console.log(...arr1.sort((a, b) => a - b))
 }
-let arr1sort
-for(let i=0;i<ar2;i++){
-    arr2[i] = parseInt(arr2[i])
-}
-console.log(arr1.sort((a, b) => a - b))
-console.log(arr2.sort((a, b) => a - b))
