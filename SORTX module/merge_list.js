578154ef2adbfcbf3a213d8c8320c2ff5d9d8cbb
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-11-10 12:30:16 +0530

    code

---
+++ SORTX module/merge_list.js
@@ -0,0 +1,26 @@
+let fs = require("fs");
+let data = fs.readFileSync('./in.txt', 'utf-8');
+let idx = 0;
+data = data.split('\n');
+function readLine() {
+    idx++;
+    return data[idx - 1].trim();
+}
+// -------- Do NOT edit anything above this line ----------
+// 3
+// 4
+// 2 6 12 34 
+// 1 9 20 1000 
+// 23 34 90 2000
+
+let t = parseInt(readLine())
+let n = parseInt(readLine())
+for (let i = 0; i < t; i++) {
+    let arr1 = readLine().split(' ')
+    for (let j = 0; j < n; j++) {
+        arr1[j] = parseInt(arr1[j])
+        
+    }
+    console.log(arr1)
+}
+
