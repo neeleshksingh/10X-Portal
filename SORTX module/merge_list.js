78b284bc6e4df7f3c539e56596eca312058dd961
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-11-10 20:40:03 +0530

    code

--- SORTX module/merge_list.js
+++ SORTX module/merge_list.js
@@ -13,30 +13,11 @@ function readLine() {
 // 1 9 20 1000 
 // 23 34 90 2000
 
-
-function mergeKArrays(arr , a,  output)
-    {
-        var c = 0;
-        for (i = 0; i < a; i++) {
-            for (j = 0; j < 4; j++)
-                output = arr[i][j];
-        }
-        return output.sort((a,b)=>a-b);
-    }
-    function printArray(arr , size) {
-        for (i = 0; i < size; i++)
-            console.log(arr[i] + " ");
-    }
 let t = parseInt(readLine())
 let n = parseInt(readLine())
+let arr = []
 for (let i = 0; i < t; i++) {
-    let arr1 = readLine().split(' ')
-    for (let j = 0; j < n; j++) {
-        arr1[j] = parseInt(arr1[j])
-    }
-    mergeKArrays(arr1, t, output);
-    var output = Array(t * n).fill(0);
-console.log(printArray(output, t * n));
+   arr[i] = readLine().split(' ').map(Number)
 }
-
+console.log(arr)
 
