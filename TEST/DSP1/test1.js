610acf30cd158f7b84e9887620437c83d08699d1
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-11-12 11:17:53 +0530

    code

---
+++ TEST/DSP1/test1.js
@@ -0,0 +1,10 @@
+let fs = require("fs");
+let data = fs.readFileSync('./in1.txt', 'utf-8');
+let idx = 0;
+data = data.split('\n');
+
+function readLine() {
+    idx++;
+    return data[idx - 1].trim();
+}
+// -------- Do NOT edit anything above this line ----------
---
+++ TEST/DSP1/test2.js
@@ -0,0 +1,10 @@
+let fs = require("fs");
+let data = fs.readFileSync('./in2.txt', 'utf-8');
+let idx = 0;
+data = data.split('\n');
+
+function readLine() {
+    idx++;
+    return data[idx - 1].trim();
+}
+// -------- Do NOT edit anything above this line ----------
