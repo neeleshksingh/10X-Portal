5a53c9e1b637c469bf280ca580708863b730e0e3
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-11-12 13:33:39 +0530

    code

--- TEST/DSP1/test1.js
+++ TEST/DSP1/test1.js
@@ -8,3 +8,27 @@ function readLine() {
     return data[idx - 1].trim();
 }
 // -------- Do NOT edit anything above this line ----------
+function max(b,n1,m1)
+{
+
+    let max=0
+    for(let i=0;i<n1.length;i++)
+    {
+        for(let j=0;j<m1.length;j++)
+        {
+            if(n1[i]+m1[j]<=b)
+            {
+                max=Math.max(max,n1[i]+m1[j])
+            }
+        }
+    }
+    if(max==0)
+    {
+        return -1
+    }
+    return max
+}
+let [b,keyboard,usb]= readLine().split(' ').map(Number)
+let n1=readLine().split(' ').map(Number)
+let m1=readLine().split(' ').map(Number)
+console.log(max(b,n1,m1))
--- TEST/DSP1/test2.js
+++ TEST/DSP1/test2.js
@@ -8,3 +8,31 @@ function readLine() {
     return data[idx - 1].trim();
 }
 // -------- Do NOT edit anything above this line ----------
+function median(arr)
+{
+    arr=arr.sort((a,b)=>a-b)
+    
+    return arr[parseInt(arr.length/2)]
+}
+let t=parseInt(readLine())
+
+while (t--)
+{
+    let arr=[]
+    let [n,m]=readLine().split(' ').map(Number)
+    for(let i=0;i<n;i++)
+    {
+        arr[i]=readLine().split(' ').map(Number)
+    }
+    
+    for(let i=0;i<arr.length;i++)
+    {
+        for(let j=0;j<arr[i].length;j++)
+        {
+            arr.push(arr[i][j])
+           
+        }
+    }
+    arr.splice(0,n)
+    console.log(median(arr))
+}
