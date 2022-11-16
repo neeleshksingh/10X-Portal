4aafaad7a51ba977ef2e4168e1d4ae9949f60bd5
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-11-16 15:48:41 +0530

    node

--- SORTX module/insert_target.js
+++ SORTX module/insert_target.js
@@ -11,7 +11,19 @@ function readLine() {
 // 1
 // 5 11
 // -2 4 5 10 15
+let t = parseInt(readLine())
+let n, q
+for (let i = 0; i < t; i++) {
+    [n, q] = readLine().split(" ").map((n) => parseInt(n))
+    //console.log(n,q)
+    let arr1 = readLine().split(' ')
+    for (let j = 0; j < n; j++) {
+        arr1[j] = parseInt(arr1[j])
+    }
+    console.log(...arr1.concat(q).sort((a, b) => a - b))
+}
 
+/*
 let t = parseInt(readLine())
 let n, q
 for (let i = 0; i < t; i++) {
@@ -22,5 +34,5 @@ let arr2 = []
 for (let i = 0; i < n; i++) {
     arr2.push(arr[i])
 }
-console.log(...arr2.concat(q).sort((a, b) => a - b))
+console.log(...arr2.concat(q).sort((a, b) => a - b))*/
 
