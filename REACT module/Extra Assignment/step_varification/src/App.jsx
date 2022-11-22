f29c4feb0766c51525358888d041c52c4b7f3a56
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-11-22 22:22:32 +0530

    code

---
+++ REACT module/Extra Assignment/step_varification/src/App.jsx
@@ -0,0 +1,9 @@
+import Hello from './welcome';
+const App = ()=> {
+  return (
+    <div>
+      <Hello/>
+    </div>
+  )
+}
+export default App;
---
+++ REACT module/Extra Assignment/step_varification/src/welcome.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+const Hello = () =>{
+    return(
+        <div>
+            Yay! I can create a boilerplate react app on my own
+        </div>
+    )
+}
+export default Hello
