905a00b26144941900f3f896e0a7d1399b0fafe4
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-11-22 21:37:29 +0530

    code

--- REACT module/Assignment 2/boilerplate-react-functional-public-main/src/components/App.js
+++ REACT module/Assignment 2/boilerplate-react-functional-public-main/src/components/App.js
@@ -1,11 +1,11 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
-
+import Para from "./para";
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="root">
+      <Para/>
+    </div>
   )
 }
-
-
 export default App;
---
+++ REACT module/Assignment 2/boilerplate-react-functional-public-main/src/components/para.js
@@ -0,0 +1,7 @@
+import React from "react";
+const Para = () =>{
+    return(
+        <p>I am learning React. My life is getting better.</p>
+    )
+} 
+export default Para
