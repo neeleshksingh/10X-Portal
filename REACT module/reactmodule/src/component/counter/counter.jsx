dd3027c4b3ccd43cc5bc4c4e26c26f4076e33f09
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-11-22 19:57:02 +0530

    code

--- REACT module/reactmodule/src/component/counter/counter.jsx
+++ REACT module/reactmodule/src/component/counter/counter.jsx
@@ -1,16 +1,22 @@
+import {useState} from "react"
 const Counter = () =>{
-    let count = 0
+    const [count, updateCount] = useState(0)
     const handleIncrement = () =>{
-
+        //update the count value
+        updateCount(count+1)
+        console.log(count)
     }
     const handleDecrement = () =>{
-
+        //update the count value
+        updateCount(count-1)
+        console.log(count)
     }
     return (
         <div>
             <button onClick={handleIncrement}>+</button>
                 <div>{count}</div>
             <button onClick={handleDecrement}>-</button>
+            <button onClick={updateCount}>Multiply by 2</button>
         </div>
     )
 }
