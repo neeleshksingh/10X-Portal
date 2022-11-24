0e34626cfcfedc40ec9c3ea2b27b74a6be689048
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-11-24 21:03:58 +0530

    code

--- REACT module/Assignment 4/Rendering-Multiple-Components-boilerplate-main/src/components/project.js
+++ REACT module/Assignment 4/Rendering-Multiple-Components-boilerplate-main/src/components/project.js
@@ -3,7 +3,7 @@ const Project = () =>{
     return(
         <div>
             <h1 data-ns-test="project-name">Free Nuclear codes</h1>
-            <p data-ns-test="project-description">Pehle ban jaye tab dekhenge!</p>
+            <p data-ns-test="project-description">The Open-source Nuclear Codes for Reactor Analysis (ONCORE) initiative is an IAEA-facilitated international collaboration framework for the development and application of open-source multi-physics simulation tools to support research, education and training for the analysis of advanced nuclear power reactors. Institutions and individuals participating in ONCORE can collaborate in, and benefit from, the development of open-source software in the field of nuclear science and technology.</p>
         </div>
     )
 }
