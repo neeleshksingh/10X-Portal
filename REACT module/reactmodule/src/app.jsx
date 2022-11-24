ed8e2fc8d3ec9538342d2aee9266c79d94b4baa3
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-11-24 19:02:22 +0530

    code

--- REACT module/reactmodule/src/app.jsx
+++ REACT module/reactmodule/src/app.jsx
@@ -1,5 +1,5 @@
-// import Welcome from "./welcome"
-// import Hello from "./hello"
+import Welcome from "./welcome"
+import Hello from "./hello"
 import Home from "./home/home"
 import Counter from "./component/counter/counter"
 import Event from "./component/Event/event"
