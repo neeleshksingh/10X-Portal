8d2fd6ad74c69a5f7f4636f5919d898cea0dc0ce
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-11-25 19:50:25 +0530

    code

--- REACT module/reactmodule/src/app.jsx
+++ REACT module/reactmodule/src/app.jsx
@@ -4,15 +4,17 @@ import Home from "./home/home"
 import Counter from "./component/counter/counter"
 import Event from "./component/Event/event"
 import Form from "./component/forms/form"
+import Calculator from "./component/calculator/calculator"
 const App = () => {
     return (
         <div>
             {/* <Welcome/>
             <Hello/> */}
             {/* <Home/> */}
-            <Counter/>
+            {/* <Counter/> */}
             {/* <Event/>
             <Form/> */}
+            <Calculator/>
         </div>
     )
 }
---
+++ REACT module/reactmodule/src/component/calculator/operation/operation.jsx
@@ -0,0 +1,15 @@
+const Operation = () =>{
+    const handleAdd = () =>{
+        
+    }
+    const handleMultiply = () =>{
+        
+    }
+    return (
+        <div>
+           <button onClick={handleAdd}>Add</button>
+           <button onClick={handleMultiply}>Multiply</button>
+        </div>
+    )
+}
+export default Operation
