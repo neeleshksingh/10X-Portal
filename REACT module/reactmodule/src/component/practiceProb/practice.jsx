1356dd3b625c3baf78b633b8d3133b3f4c9a01d4
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-11-26 12:38:47 +0530

    code

--- REACT module/reactmodule/src/component/practiceProb/practice.jsx
+++ REACT module/reactmodule/src/component/practiceProb/practice.jsx
@@ -4,18 +4,25 @@ const Practice = () =>{
     const [item, setItem] = useState("")
     const [userList, setUserList] = useState([])
     const [age, setAge] = useState(0)
+    const [category, setCategory] = useState("")
     const handleItemAdd = () => {
         if(age > 18){
-            setUserList([...userList,{name: item, age: age}])
+            setUserList([...userList,{name: item, age: age, category: category}])
         }
         setItem("")
         setAge(0)
+        setCategory("")
     }
     return(
         <div>
             <div>You are minor</div>
             <input onChange={(event)=>{setItem(event.target.value)}} value={item}/>
             <input type="number" onChange={(event)=>{setAge(parseInt(event.target.value))}} value={age}/>
+            <select onChange={(event)=>{setCategory(event.target.value)}} value={category}>
+                <option>Choose your category</option>
+                <option value="veg">Veg</option>
+                <option value="non-veg">Non-Veg</option>
+            </select>
             <button onClick={handleItemAdd}>Add Item</button>
             <ul>
                 {userList.map((user, index) =>{
@@ -23,6 +30,7 @@ const Practice = () =>{
                         <div key={index}>
                         <li>{user.name}</li>
                         <li>{user.age}</li>
+                        <li>{user.category}</li>
                         </div>
                     )
                 })}
