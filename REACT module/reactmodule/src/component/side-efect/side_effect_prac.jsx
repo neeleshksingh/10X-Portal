73494fc4ca5b45bf85fad0334a246f68413c7a06
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-11-27 12:50:42 +0530

    code

--- REACT module/reactmodule/src/component/side-efect/side_effect_prac.jsx
+++ REACT module/reactmodule/src/component/side-efect/side_effect_prac.jsx
@@ -2,23 +2,48 @@ import axios from "axios"
 import { useState, useEffect } from "react"
 const SideEffectPractise = () =>{
     const [bitcoinQuantity, setBitcoinQuantity] = useState(0)
-    useEffect(()=>{}, [])
+    const [cryptoData,setCryptoData] = useState([])
+    useEffect(()=>{
+        getPriceData()
+    }, [])
     const getPriceData = () =>{
         axios.get("https://api.coindesk.com/v1/bpi/currentprice.json").then((bitcoinData)=>{
-            console.log(bitcoinData)
+        setCryptoData(Object.values(bitcoinData.data.bpi))    
+        console.log(cryptoData, "crypto")
         }).catch(()=>{
 
         }).finally(()=>{
 
         })
     }
-    getPriceData()
+    //getPriceData()
     return(
         <div>
             <h2>Application for Side Effect</h2>
             <button onClick={() =>{setBitcoinQuantity(bitcoinQuantity + 1)}}>+</button>
             <button onClick={() =>{setBitcoinQuantity(bitcoinQuantity - 1)}}>-</button>
             <div>Quantity: {bitcoinQuantity}</div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Code</th>
+                        <th>Price</th>
+                        <th>Description</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {!cryptoData.length && <img src="{loader}" alt="loading"/>}
+                    {cryptoData.map((value, index) =>{
+                        return(
+                            <tr key="index">
+                                <td>{value.code}</td>
+                                <td>{value.rate}</td>
+                                <td>{value.description}</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
         </div>
     )
 }
