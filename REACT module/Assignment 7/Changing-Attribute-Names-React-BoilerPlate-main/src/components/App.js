2a04251e4f2d0024daaee0a82d01e94ed091ee25
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-11-27 11:11:19 +0530

    code

---
+++ REACT module/Assignment 7/Changing-Attribute-Names-React-BoilerPlate-main/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+import '../styles/App.css';
+
+const App = () => {
+
+  const [count, setCount] = useState(0)
+
+  return (
+    <div class="ball">
+      <h1 class="count" onDoubleClick={() => { alert("cant edit it") }}>{count}</h1>
+      <button class='increment-button' onClick={() => { setCount(count + 1) }}>Increment</button>
+    </div>
+  )
+}
+export default App;
