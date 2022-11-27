d2971579ea071e58dbbdd4f236048ed4ae34bbef
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-11-27 12:00:39 +0530

    code

---
+++ REACT module/Assignment 8/boilerplate-react-functional-public-main/src/components/form.js
@@ -0,0 +1,29 @@
+import React from "react";
+import '/form.css'
+const Form = () =>{
+    return(
+        <div>
+            <h1>Signup Form</h1>
+            <form>
+                <div className="form1">
+                <label for="name">Name</label>
+                <input type="text" name="name" data-testid ='name' id="name" required/> <br/>
+                <label for="email">Email Address</label>
+                <input type="email" name="email" data-testid="email" id="email" required/><br/>
+                <label for="gender">Gender</label>
+                <select data-testid ="gender" required>
+                    <option>Select Option</option>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                </select><br/>
+                <label for="phoneNumber">Phone Number</label>
+                <input type="number" name="phoneNumber" data-testid='phoneNumber' id="phoneNumber" required/><br/>
+                <label for="password">Password</label>
+                <input type="password" name="password" data-testid="password" id="password" required/>
+                </div>
+                <button type="submit" data-testid= 'submit' id="submit">Submit</button>
+            </form>
+        </div>
+    )
+}
+export default Form
