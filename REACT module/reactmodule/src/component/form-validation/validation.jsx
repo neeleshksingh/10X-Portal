4f411b60b24acf0fe97fe8f6f096fd6fb85b82a3
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-11-28 21:38:21 +0530

    code

--- REACT module/reactmodule/src/component/form-validation/validation.jsx
+++ REACT module/reactmodule/src/component/form-validation/validation.jsx
@@ -1,25 +1,31 @@
+import { useState } from "react"
+
 const Validation = () =>{
+    const [form, setForm] = useState({username:"", email:"", password:"", confirmPassword: ""})
+    const handleSubmit = (event) =>{
+        event.preventDefault()
+    }
     return(
         <div>
             <form action="">
                 <div>
                     <label htmlFor="username">Username</label>
-                    <input type="text" id="username" />
+                    <input type="text" id="username" onChange={(event)=>{setForm({...form, username: event.target.value})}} onBlur={(event)=>{}} value={form.username}/>
                 </div>
                 <div>
                     <label htmlFor="email">Email</label>
-                    <input type="email" name="email" id="email" />
+                    <input type="email" name="email" id="email" onChange={(event)=>{setForm({...form, email: event.target.value})}} onBlur={(event)=>{}} value={form.email} />
                 </div>
                 <div>
                     <label htmlFor="password">Password <span style={{color:"red"}}>*</span> </label>
-                    <input type="password" id="password" />
+                    <input type="password" id="password" onChange={(event)=>{setForm({...form, password: event.target.value})}} onBlur={(event)=>{}} value={form.password}/>
                 </div>
                 <div>
                     <label htmlFor="confirm-password">Confirm Password</label>
-                    <input type="password" name="confirm-password" id="confirm-password" />
+                    <input type="password" name="confirm-password" id="confirm-password" onChange={(event)=>{setForm({...form, confirmPassword: event.target.value})}} onBlur={(event)=>{}} value={form.confirmPassword}/>
                 </div>
+                <button type="submit" onClick={handleSubmit}>Submit</button>
             </form>
-            <button type="submit">Submit</button>
         </div>
     )
 }
