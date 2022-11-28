69af98d90a530b14b047dc72a20e9765acd39166
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-11-28 12:52:33 +0530

    code

--- REACT module/Assignment 8/boilerplate-react-functional-public-main/src/components/form.js
+++ REACT module/Assignment 8/boilerplate-react-functional-public-main/src/components/form.js
@@ -1,6 +1,45 @@
 import React from "react";
 import './form.css'
+import { useState } from "react";
 const Form = () =>{
+    const [data, updatedata] = useState({
+        user: "",
+        email: "",
+        gender: "",
+        phone: "",
+        pass: "",
+      });
+      const [massage, updatedmassage] = useState({
+        nothing: "",
+        user: "",
+        email: "",
+        phone: "",
+        pass: "",
+        display: "",
+      });
+      const submit = () => {
+        if (
+          data.user == "" ||
+          data.email == "" ||
+          data.phone == "" ||
+          data.pass == ""
+        ) {
+          updatedmassage({ nothing: "All fields are mandatory" });
+        } else if (!data.user.match(/^[0-9a-z ]+$/i)) {
+          updatedmassage({ user: "Name is not alphanumeric" });
+        } else if (!data.email.includes("@")) {
+          updatedmassage({ email: "Email must contain @" });
+        } else if (!data.phone.match(/^[0-9]+$/i)) {
+          updatedmassage({ phone: "Phone Number must contain only numbers" });
+        } else if (data.pass.length < 6) {
+          updatedmassage({ pass: "Password must contain atleast 6 letters" });
+        } else {
+          for (let i = 0; i < data.email.length; i++) {
+            if (data.email[i] == "@")
+              updatedmassage({ display: `Hello ${data.email.slice(0, i)}` });
+          }
+        }
+      };
     return(
         <div className="container">
             <h1>Signup Form</h1>
