10210223e64d9843a5bc26a6a8d9f27bd1b175d5
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-11-29 20:05:27 +0530

    code

--- REACT module/reactmodule/src/component/use-reduce-hook/useReducer.jsx
+++ REACT module/reactmodule/src/component/use-reduce-hook/useReducer.jsx
@@ -15,17 +15,17 @@ const UseReducer = () =>{
                 return currentValue
         }
     }
-    const initialState = 0
+    const initialState = {counter: 0, updateBy: 0}
     //const [counter, setCounter] = useState(0)
-    const [counter, dispatch] = useReducer(reducer, initialState)
-    const [inputValue, setInputValue] = useState("")
+    const [counterApp, dispatch] = useReducer(reducer, initialState)
+    //const [inputValue, setInputValue] = useState("")
     return(
         <div>
             <button onClick={()=>{dispatch({type: "Increment"})}}>+</button>
-            <div>{counter}</div>
+            <div>{counterApp.counter}</div>
             <button onClick={()=>{dispatch({type: "Decrement"})}}>-</button>
-            <input type="text" onChange={(e) => {setInputValue(e.target.value)}} />
-            <button onClick={() => {dispatch({type: "IncrementByValue", value: parseInt(counterApp.update)})}}>Increment By</button>
+            <input  onChange={(e) => {dispatch({type: "changeValue", value: e.target.value})}} />
+            <button onClick={() => {dispatch({type: "IncrementByValue", value: parseInt(counterApp.updateBy)})}}>Increment By</button>
         </div>
     )
 }
