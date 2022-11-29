420b8ca21875a80d27c482485d4aefbee1f5e4c4
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-11-29 12:43:59 +0530

    code

--- REACT module/reactmodule/src/component/form-validation/validation.jsx
+++ REACT module/reactmodule/src/component/form-validation/validation.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-
+import './validation.css'
 const Validation = () =>{
     const [form, setForm] = useState({username:"", email:"", password:"", confirmPassword: ""})
     const [error, setError] = useState({username:{isValid : true, message:""}, email:{isValid : true, message:""}, password:{isValid : true, message:""}, confirmPassword:{isValid : true, message:""}})
@@ -28,23 +28,26 @@ const Validation = () =>{
     }
     const isSubmitValid = form.username.length && form.email.length && form.password.length && form.confirmPassword.length
     return(
-        <div>
-            <form action="">
+        <div id="container">
+            <div>
+                <h1 id="heading">Registration Form</h1>
+            </div>
+            <form action="" id="form">
                 <div>
-                    <label htmlFor="username">Username</label>
+                    <label htmlFor="username">Username<span style={{color:"red"}}>*</span></label><br />
                     <input type="text" id="username" onChange={(event)=>{setForm({...form, username: event.target.value})}} onBlur={(event)=>{checkErrors("username")}} value={form.username}/>
                     {!error.username.isValid? <div>{error.message}</div> : null}
                 </div>
                 <div>
-                    <label htmlFor="email">Email</label>
+                    <label htmlFor="email">Email<span style={{color:"red"}}>*</span></label><br />
                     <input type="email" name="email" id="email" onChange={(event)=>{setForm({...form, email: event.target.value})}} onBlur={(event)=>{checkErrors("email")}} value={form.email} />
                 </div>
                 <div>
-                    <label htmlFor="password">Password <span style={{color:"red"}}>*</span> </label>
+                    <label htmlFor="password">Password <span style={{color:"red"}}>*</span> </label><br />
                     <input type="password" id="password" onChange={(event)=>{setForm({...form, password: event.target.value})}} onBlur={(event)=>{checkErrors("password")}} value={form.password}/>
                 </div>
                 <div>
-                    <label htmlFor="confirm-password">Confirm Password</label>
+                    <label htmlFor="confirm-password">Confirm Password<span style={{color:"red"}}>*</span></label><br />
                     <input type="password" name="confirm-password" id="confirm-password" onChange={(event)=>{setForm({...form, confirmPassword: event.target.value})}} onBlur={(event)=>{checkErrors("confirPassword")}} value={form.confirmPassword}/>
                 </div>
                 <button type="submit" disabled={isSubmitValid === 0 ? true : false} onClick={handleSubmit}>Submit</button>
