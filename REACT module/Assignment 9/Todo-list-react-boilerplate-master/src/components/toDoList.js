e332803888776c0cf9e8bf8d3f33fefde8b8247a
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-11-30 23:32:47 +0530

    code

--- REACT module/Assignment 9/Todo-list-react-boilerplate-master/src/components/toDoList.js
+++ REACT module/Assignment 9/Todo-list-react-boilerplate-master/src/components/toDoList.js
@@ -1,11 +1,39 @@
+import { useState } from "react"
 import React from "react"
+import './todo.css'
 
-const ToDoList = () =>{
+const ToDoList = ()=>{
+const [inp,setInp]=useState('')
+const [arr,setArr]=useState([])
+const handleData = ()=>{
+    console.log(inp,arr)
+    if(inp!='')
+    {
+     setArr([...arr,inp])
+     setInp('')
+    }
+}
+const deleteData= (deletevalue) =>{
+    console.log(deletevalue)
+    let x=arr.filter((data)=> data!= deletevalue)
+    setArr(x)
+}
     return(
-        <div>
-            <input id="task" placeholder="Enter the task"/>
-            <button id="btn">Add Task</button>            
+        <section className="container">
+        <h1 className="heading">Website Todo</h1>
+        <div className="box">
+        <input id="task" placeholder="Enter items here" className='inp' onChange={(e)=>{setInp(e.target.value)}} value={inp}/>
+        
+        <ul className="list">
+            {arr.map((data,index)=>{return( 
+                <div className="li-cont">
+                 <li>{data}</li> 
+                 <button className='del' onClick={()=>{deleteData(data)}}>Delete</button>
+                 </div>
+            )})}
+        </ul>
+        <button id="btn" className='btn' onClick={handleData}>Add New Task</button>
         </div>
-    )
+    </section>)
 }
 export default ToDoList
