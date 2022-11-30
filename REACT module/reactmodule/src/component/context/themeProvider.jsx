14ae1e2c92a699b1331e8997769737371b2e540b
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-11-30 19:08:31 +0530

    code

---
+++ REACT module/reactmodule/src/component/context/themeProvider.jsx
@@ -0,0 +1,10 @@
+import { ThemeContext } from "./themeContext"
+
+const ThemeProvider = () =>{
+    return(
+        <ThemeContext.Provider>
+
+        </ThemeContext.Provider>
+    )
+}
+export default ThemeProvider
