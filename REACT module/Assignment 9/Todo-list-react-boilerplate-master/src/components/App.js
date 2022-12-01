c98735c2cdd3652f4ae0061ddf2ee2daa0f7ee7f
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-12-01 12:20:41 +0530

    code

--- REACT module/Assignment 9/Todo-list-react-boilerplate-master/src/components/App.js
+++ REACT module/Assignment 9/Todo-list-react-boilerplate-master/src/components/App.js
@@ -8,8 +8,6 @@ function App()
 {
 	return (
 	<div id="main">
-	//Do not alter main div
-	//Please do not alter the functional component as tests depend on the type of component.
 	<ToDoList/>
 	</div>
 	);
