b4884e70b97f275a7beb54403d59057a062d27ff
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-12-03 13:15:43 +0530

    code

--- REACT module/reactmodule/src/component/Ref/refComplex.jsx
+++ REACT module/reactmodule/src/component/Ref/refComplex.jsx
@@ -1,8 +1,11 @@
+import { useRef } from "react"
+
 const RefComplex = () => {
+    const form = useRef({username : '', password: ''})
     return(
         <>
-            <input type="text" />
-            <input type="password" />
+            <input type="text" onChange={(e)=>form.current.username = e.target.value}/>
+            <input type="password" onChange={(e)=>form.current.password = e.target.value}/>
             <button onClick={()=>{}}>Login</button>
         </>
     )
