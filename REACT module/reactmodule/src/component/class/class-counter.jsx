2102a674f624b355595a95b530772e325e46a1af
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-12-04 16:32:54 +0530

    code

---
+++ REACT module/reactmodule/src/component/class/class-counter.jsx
@@ -0,0 +1,17 @@
+// by using function
+// const Counter = () =>{
+//     return(
+//         <>
+        
+//         </>
+//     )
+// }
+
+//by using class
+import React from "react";
+class Counter extends React.Component {
+    constructor() {
+        super()
+    }
+}
+export default Counter
