4ca85682957dbaa62b8ab7257a36a2719bf098db
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-12-05 19:09:09 +0530

    code

--- REACT module/reactmodule/src/component/performance-optimisation/performance.jsx
+++ REACT module/reactmodule/src/component/performance-optimisation/performance.jsx
@@ -4,16 +4,18 @@ import PerformanceChild from "./performance-child"
 const Performance = () =>{
     const [list, setList] = useState([])
     const [listItem, setListItem] = useState('')
+    const [heading, setHeading] = useState("I am old Heading")
     return(
         <>
             <input onChange={(e)=>{setListItem(e.target.value)}} value={listItem} type="text" />
             <button onClick={()=>{setList([...list, listItem]); setListItem("")}}>Add to list</button>
+            <button onClick={()=>{setHeading("I am updated Heading")}}>Update Heading</button>
             {list.map((value, key)=>{
                 return(
                     <div key={key}>{value}</div>
                 )
             })}
-            <PerformanceChild heading = "Data from Parent"/>
+            <PerformanceChild heading = {heading}/>
         </>
     )
 }
