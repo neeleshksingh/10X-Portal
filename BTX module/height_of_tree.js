99422ed02e471c9f71d136fe766474a9827e671b
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-12-07 11:30:53 +0530

    node

---
+++ BTX module/height_of_tree.js
@@ -0,0 +1,77 @@
+let fs = require("fs");
+let data = fs.readFileSync(0, 'utf-8');
+let idx = 0;
+data = data.split('\n');
+
+function readLine() {
+    idx++;
+    return data[idx - 1].trim();
+}
+
+// -------- Do NOT edit anything above this line ----------
+// Use readLine() for taking input, it will read one line of from the input  and is stored in string format
+
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinaryTree {
+    constructor() {
+        this.root = null;
+    }
+
+    height(root) {
+
+        // write code here
+        let depth = 0
+
+        let q = []
+        q.push(root)
+        q.push(null)
+        while (q.length > 0) {
+            let temp = q.shift()
+            if (temp == null)
+                depth += 1
+            if (temp != null) {
+                if (temp.left)
+                    q.push(temp.left)
+
+                if (temp.right)
+                    q.push(temp.right)
+            }
+            else if (q.length > 0)
+                q.push(null)
+        }
+        return depth
+    }
+
+    insertLevelOrder(arr, i, n) {
+        if (i >= n) {
+            return null;
+        }
+        let tree = new Node(arr[i]);
+        tree.left = this.insertLevelOrder(arr, 2 * i + 1, n);
+        tree.right = this.insertLevelOrder(arr, 2 * i + 2, n);
+        return tree;
+    }
+}
+
+function convertToNumber(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== "N") {
+            arr[i] = parseInt(arr[i]);
+        } else {
+            arr[i] = null;
+        }
+    }
+}
+
+let arr = readLine().split(" ");
+convertToNumber(arr);
+let tree = new BinaryTree();
+tree.root = tree.insertLevelOrder(arr, 0, arr.length)
+console.log(tree.height(tree.root))
