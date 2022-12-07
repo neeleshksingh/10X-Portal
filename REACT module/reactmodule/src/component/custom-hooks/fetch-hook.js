0bda96c1e6e7af29a5a505e6d492715c8600923a
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-12-07 18:59:15 +0530

    code

--- REACT module/reactmodule/src/component/custom-hooks/fetch-hook.js
+++ REACT module/reactmodule/src/component/custom-hooks/fetch-hook.js
@@ -1,11 +1,13 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function useFetch(url){
+    const [data, setData] = useState(null)
     useEffect(()=>{
         fetch(url).then((res)=>{
             return res.json()
         }).then((data)=>{
-            
+            setData(data)
         })
     },[url])
+    return data
 }
