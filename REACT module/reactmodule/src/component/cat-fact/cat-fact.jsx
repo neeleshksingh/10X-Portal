0e08d76da291faf4409bacc0ee786c970368bf00
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-12-07 19:20:41 +0530

    code

--- REACT module/reactmodule/src/component/cat-fact/cat-fact.jsx
+++ REACT module/reactmodule/src/component/cat-fact/cat-fact.jsx
@@ -1,6 +1,8 @@
-import { useFetch } from "../custom-hooks/fetch-hook"
 import { useEffect } from "react"
+import { useFetch } from "../cat-fact"
 const CatFact = () =>{
+    const data = useFetch("https://catfact.ninja/fact")
+    console.log(data)
     // useEffect(()=>{
     //     fetch("https://catfact.ninja/fact").then((res)=>{
     //         return res.json()
