ead624716c1d03c9ce2da9c54d0e672f3ac2f68f
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-12-18 09:42:24 +0530

    code

--- NODEJS module/http.js
+++ NODEJS module/http.js
@@ -1,9 +1,39 @@
 const http = require("http")
 
 const server = http.createServer((req, res)=>{
-    res.writeHead(200, {"Content-type" : "text/html"})
-    res.write(`<h1>Welcome to node JS !</h1> <p>This is our first http server</p>`)
-    res.end()
+    console.log(req.url, req.method, req.headers);
+    if(req.url == "/"){
+        res.writeHead(200, {
+            "Content-type" : "text/html",
+            "Keep-Alive" : 100
+        })
+        res.write(`<h1>Welcome to node JS !</h1> <p>This is our first http server</p>`)
+        res.end()
+    }
+    else if(req.url == "/contactus"){
+        res.writeHead(200, {
+            "Content-type" : "text/html",
+            "Keep-Alive" : 100
+        })
+        res.write(`<h1>Welcome to Contact US !</h1> <p>This is our first Contact us server</p>`)
+        res.end()
+    }
+    else if(req.url == "/aboutus"){
+        res.writeHead(200, {
+            "Content-type" : "text/html",
+            "Keep-Alive" : 100
+        })
+        res.write(`<h1>About US</h1>`)
+        res.end()
+    }
+    else {
+        res.writeHead(404, {
+            "Content-type" : "text/html",
+            "Keep-Alive" : 100
+        })
+        res.write(`<h1>Page is not found !</h1>`)
+        res.end()
+    }
 })
 
 server.listen(3000, ()=> console.log("Server is up at port no 3000"))
