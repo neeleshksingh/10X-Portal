d36cdf00881469f0aaf5880284779cd3ddc96dd7
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-12-18 09:31:46 +0530

    code

--- NODEJS module/http.js
+++ NODEJS module/http.js
@@ -1,7 +1,9 @@
 const http = require("http")
 
 const server = http.createServer((req, res)=>{
-    res.end("Welcome to node JS")
+    res.writeHead(200, {"Content-type" : "text/html"})
+    res.write(`<h1>Welcome to node JS !</h1> <p>This is our first http server</p>`)
+    res.end()
 })
 
 server.listen(3000, ()=> console.log("Server is up at port no 3000"))
