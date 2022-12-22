ab3cf7df3b817dfd57818635553f532a6c909290
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-12-22 20:45:57 +0530

    code

--- NODEJS module/Assignments/node-express-boilerplate-public-main/src/index.js
+++ NODEJS module/Assignments/node-express-boilerplate-public-main/src/index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const bodyParser = require("body-parser");
 const port = 3000
+
+
+
 app.use(express.urlencoded());
 
 // Parse JSON bodies (as sent by API clients)
@@ -12,8 +15,18 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
 // your code goes here
-
-
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+app.get('', (req,res)=>{
+    res.send('Hello World !')
+})
+app.get('/calc', (req,res)=>{
+    res.sendFile(__dirname+"/index.html")
+})
+app.post('/add', (req, res)=>{
+    const n1 = Number(req.body.num1)
+    const n2 = Number(req.body.num2)
+    const add = Number(n1+n2)
+    res.send(JSON.stringify('the sum of given two numbers '+ add))
+})
+app.listen(3000, (res) => {console.log(`App listening on port ${port}!`)})
 
 module.exports = app;
--- NODEJS module/Lecture/ExpressJS/app.js
+++ NODEJS module/Lecture/ExpressJS/app.js
@@ -12,37 +12,59 @@ console.log(cowsay.say({
 
 const express = require('express')
 // const faker = require('faker')
+var bodyParser = require('body-parser')
+const multer = require('multer')
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.set('views', './views')
 app.set('view engine', 'ejs')
 app.use(express.static("public"))
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './public')
+    },
+    filename: function (req, file, cb) {
+        const filename = file.filename + "-" + Date.now()
+        cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+})
+
+const upload = multer({ storage: storage })
+
 const userArr = []
 
 // for(let i=0;i<10;i++){
-//     userArr.push({
-//         name : faker.name.findName(),
-//         email : faker.internet.email(),
-//         image: faker.image.image()
-//     })
+//      userArr.push({
+//          name : faker.name.findName(),
+//          email : faker.internet.email(),
+//          image: faker.image.image()
+//      })
 // }
 
 
-app.get("/", (req, res)=>{
-    res.render("user.ejs", {user: userArr[0]})
+app.get("/", (req, res) => {
+    res.render("form.ejs", { user: userArr[0] })
 })
 
-app.get("/user", (req, res)=>{
+app.get("/add/user", upload.single('profilepic'), (req, res) => {
     console.log(req.query);
     res.json({
-        name : req.query.name,
-        email : req.query.email,
-        age: req.query.age
+        name: req.query.name,
+        email: req.query.email,
+        image: req.file.filename
     })
 })
-app.get("*", (req, res)=>{
+
+app.get("/remove", (req, res) => {
+    userArr.pop()
+    res.redirect("/")
+})
+
+app.get("*", (req, res) => {
     res.status(404).send("Page is not Found")
 })
 
-app.listen(3000, ()=>console.log("Our server is up at 3000"))
+app.listen(3000, () => console.log("Our server is up at 3000"))
