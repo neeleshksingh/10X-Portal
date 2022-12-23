cbcb6106889c4057cbb896d855de0ecf18b09d63
Neelesh Kumar Singh <neelesh1517@gmail.com>
2022-12-23 21:50:09 +0530

    code

--- NODEJS module/Lecture/Mongodb/app-mongoose.js
+++ NODEJS module/Lecture/Mongodb/app-mongoose.js
@@ -1,11 +1,29 @@
-const faker = require('faker')
+//const faker = require('faker')
 const mongoose = require('mongoose')
 
-async function main(){
+async function main() {
     await mongoose.connect('mongodb://localhost/mongoose_test');
     console.log("connection done successfully");
+
+    const Schema = mongoose.Schema
+    const ObjectId = Schema.ObjectId
+
+    const userSchema = new Schema({
+        name: { type: String, require: true },
+        age: Number,
+        email: { type: String, unique: true }
+    }, { timestamps: true })
+
+    const User = mongoose.model('User', userSchema)
+
+    const data = await User.create({
+        name: "Neelesh",
+        email: "neel@j.com",
+        age: 23
+    })
+    console.log(data);
 }
 main()
     .then(console.log)
     .catch(console.error)
-    .finally(() => client.close());
+    .finally(() => mongoose.disconnect());
