0090220788a323d478adedccba8b365cc5c9cac7
Neelesh Kumar Singh <neelesh1517@gmail.com>
2023-01-02 22:12:27 +0530

    code

--- NODEJS module/Assignments/node-assignment7/student-api-server-Node-BoilerPlate-main/src/index.js
+++ NODEJS module/Assignments/node-assignment7/student-api-server-Node-BoilerPlate-main/src/index.js
@@ -10,6 +10,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 // your code goes here
+var id = studentArray.length
 app.get('/api/student', (req, res)=>{
 
    
@@ -18,6 +19,24 @@ app.get('/api/student/:id', (req, res)=>{
     const index = studentArray.findIndex(element => element.id == req.params.id)
     if(index >= 0){
         const student = studentArray[index]
+        res.json({
+            status: "success",
+            student
+        })
+    }
+    else{
+        res.status(400).json({
+            status: "failed",
+            message: 'student record not found'
+        })
+    }
+    console.log(req.params);
+    res.send('ok')
+})
+app.put('/api/student/:id', (req, res)=>{
+    const index = studentArray.findIndex(element => element.id == req.params.id)
+    if(index >= 0){
+        studentArray[index] = req.body
         res.json({
             status: "success",
             student
