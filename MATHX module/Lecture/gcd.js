612014f6796fa0a063167d3ef437497cfb085638
Neelesh Kumar Singh <neelesh1517@gmail.com>
2023-01-09 09:58:22 +0530

    code

--- MATHX module/Lecture/gcd.js
+++ MATHX module/Lecture/gcd.js
@@ -1,19 +1,30 @@
-function getGCD(a,b){
-    if (a == 0 && b==0){
+function getGCD(a, b) {
+    if (a == 0 && b == 0) {
         console.log("Infy");
         return -1
     }
-    else if(a == 0){
+    else if (a == 0) {
         return b
     }
-    else if(b == 0){
+    else if (b == 0) {
         return a
     }
-    else if(a<=b){
-        return (a, b%a)
+    else if (a <= b) {
+        return (a, b % a)
     }
-    else{
-        return (b, a%b)
+    else {
+        return (b, a % b)
     }
 }
 console.log(getGCD(63, 45));
+
+//calculate LCM
+
+let a = 35
+let b = 85
+console.log(a * b / getGCD(a, b));
+
+//GCD of three numbers
+GCD(a, b, c) = GCD(a, GCD(b, c))
+
+LCM(a, b, c) = LCM
