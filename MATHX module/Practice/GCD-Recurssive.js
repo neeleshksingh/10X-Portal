e666188e0ba823f2deeec4214be057a6351280fe
Neelesh Kumar Singh <neelesh1517@gmail.com>
2023-01-09 11:23:18 +0530

    code

--- MATHX module/Practice/GCD-Recurssive.js
+++ MATHX module/Practice/GCD-Recurssive.js
@@ -10,6 +10,31 @@ function readLine() {
 
 // -------- Do NOT edit anything above this line ----------
 // Use readLine() for taking input, it will read one line of from the input  and is stored in string format
+// GCD: Recursive
+// Description
+// // Write a program that computes the GCD of two positive integers using Euclid's algorithm. 
+// Your implementation should be recursive in nature. Given two positive integers a, b (a >= b), 
+// Euclid's algorithm is defined as follows.
+
+// // GCD(a, b) = b, if b is a divisor of a
+// //           = GCD(b, a%b), otherwise
+// // Input/Output format
+// // You only need to implement the gcd function provided in the template. 
+// The template takes care of reading the input, passing it on to gcd function and printing the result.
+
+// // Sample input
+// // 2
+// // 180 48
+// // 105 1001
+// // Sample output
+// // 12
+// // 7
+// // Explanation
+// // First line contains 2, meaning there are 2 test cases.
+// // First test case is 180 48. The greatest common divisor of 180 and 48 is 12. 
+// So, the first line of output is 12.
+// // Second test case is 105 1001. The greatest common divisor of 105 and 1001 is 7. 
+// So, the second line of output is 7.
 
 function getGCD(a, b) {
     if (a == 0 && b == 0) {
